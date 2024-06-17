06431a0b76fad58ebf45b2dcfbf7c1bcd5234932
Laurent Grimaldi <lgi@suneris.com>
2024-06-17 08:30:26 +0200

    fix crash call api not working + add yarn.lock gitignore

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -2,9 +2,11 @@ import { manakitRegistry } from '$lib/store';
 import axios from 'axios';
 
 export async function getRegistryManakit() {
-	const values = await axios.get(`https://registry.npmjs.org/manakit`);
+	try {
+		const values = await axios.get(`https://registry.npmjs.org/manakit`);
 
-	manakitRegistry.set({
-		version: values?.data['dist-tags']?.latest
-	});
+		manakitRegistry.set({
+			version: values?.data['dist-tags']?.latest
+		});
+	} catch (error) {}
 }
